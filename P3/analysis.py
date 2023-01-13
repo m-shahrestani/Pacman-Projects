4f42d5955eaff8b264141012f0e0078fd9d995bd
m-shahrestani <9728054@ceit.local>
2023-01-14 02:05:54 +0330

    Change living reward

--- P3/analysis.py
+++ P3/analysis.py
@@ -25,37 +25,37 @@ def question2():
     return answerDiscount, answerNoise
 
 def question3a():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    answerLivingReward = -3.0
+    answerDiscount = 0.9
+    answerNoise = 0.1
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
 def question3b():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    answerLivingReward = 0
+    answerDiscount = 0.2
+    answerNoise = 0.2
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
 def question3c():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    answerLivingReward = 0
+    answerDiscount = 0.9
+    answerNoise = 0
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
 def question3d():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    answerLivingReward = 0
+    answerDiscount = 0.9
+    answerNoise = 0.3
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
 def question3e():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    answerLivingReward = 15
+    answerDiscount = 0.9
+    answerNoise = 0.2
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
